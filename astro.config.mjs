70eaa6dadd5a9c793a740b0ecbb31ced1f19b93f
induvara <mahima@thegpservice.co.uk>
2025-08-06 10:40:33 +0530

    Astro Configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+    vite: {
+      resolve: {
+        alias: {
+          '@components': '/src/components',
+          '@layouts': '/src/layouts',
+          '@styles': '/src/styles',
+          '@assets': '/src/assets',
+        }
+      },
+
+      plugins: [tailwindcss()]
+    }
 });
