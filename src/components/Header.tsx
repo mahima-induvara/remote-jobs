0942bcd44f742742bdcbe7eb7a83a5ee5bf6286c
induvara <mahima@thegpservice.co.uk>
2025-10-23 19:28:07 +0530

    Coming Soon Page Added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -64,7 +64,7 @@ const Header: React.FC = () => {
   }, []);
 
   return (
-    <header className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm py-2">
+    <header className="web-header fixed inset-x-0 top-0 z-50 bg-white shadow-sm py-2">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
